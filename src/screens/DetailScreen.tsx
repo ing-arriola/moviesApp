865b772d0e74595d9482c0a19774a6feb12590cd
ing-arriola <ingeniero.arriola@gmail.com>
2021-11-25 07:26:08 -0500

    Added params to get a movie in params

--- src/screens/DetailScreen.tsx
+++ src/screens/DetailScreen.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
 import { View, Text } from 'react-native'
+import { StackScreenProps } from '@react-navigation/stack';
+import { RootStackParams } from '../navigation/MainNavigator';
 
-const DetailScreen = () => {
+interface Props extends StackScreenProps<RootStackParams,'DetailScreen'>{}
+
+const DetailScreen = ({route}:Props) => {
+    const movie=route.params
+    console.log(movie)
     return (
         <View>
             <Text>
