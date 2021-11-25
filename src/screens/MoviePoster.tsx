62864d4d63abb3f5c33794ea51624c10eca73c7f
ing-arriola <ingeniero.arriola@gmail.com>
2021-11-25 07:28:17 -0500

    Added props plus now view is a touchable opacity to allow user see specific info about a movie

--- src/screens/MoviePoster.tsx
+++ src/screens/MoviePoster.tsx
@@ -1,22 +1,35 @@
+import { useNavigation } from '@react-navigation/core';
+import { StackNavigationProp } from '@react-navigation/stack';
 import React from 'react'
-import { View, Text, Image, StyleSheet } from 'react-native'
+import { View, Text, Image, StyleSheet, Dimensions } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 import { Movie } from '../interfaces/movieDb'
+import { RootStackParams } from '../navigation/MainNavigator';
 
 interface Props{
     movie:Movie
+    height?:number
+    width?:number
+    marginH?:number
 }
 
-const MoviePoster = ({movie}:Props) => {
+const MoviePoster = ({width=200,height=400,movie,marginH=0}:Props) => {
+
+    const navigation = useNavigation<StackNavigationProp<RootStackParams>>();
+
     const uri =`https://image.tmdb.org/t/p/w500${movie.poster_path}`
     return (
-        <View style={styles.container}>
+        <TouchableOpacity
+            onPress={()=>navigation.navigate('DetailScreen' as never,movie as never)}
+            activeOpacity={0.8} 
+            style={{flex:1,height,width,marginHorizontal:marginH,paddingBottom:20,paddingHorizontal:5}}>
             <View style={styles.imageContainer} >
                 <Image
                     source={{uri}}
                     style={styles.image}
                 />
             </View>
-        </View>
+        </TouchableOpacity>
     )
 }
 
@@ -35,12 +48,7 @@ const styles = StyleSheet.create({
         },
         shadowOpacity: 0.36,
         shadowRadius: 7,
-
         elevation: 11,
-    },
-    container:{
-        width:200,
-        height:400
     }
 });
 
