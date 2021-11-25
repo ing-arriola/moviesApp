aa790e897768b030d5a667df2ef091b907228869
ing-arriola <ingeniero.arriola@gmail.com>
2021-11-25 07:25:12 -0500

    Added rootstackPrams to use type screens params

--- src/navigation/MainNavigator.tsx
+++ src/navigation/MainNavigator.tsx
@@ -2,8 +2,14 @@ import React from 'react'
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from '../screens/HomeScreen';
 import DetailScreen from '../screens/DetailScreen';
+import { Movie } from '../interfaces/movieDb';
 
-const Main = createStackNavigator();
+export type RootStackParams = {
+    Home:undefined,
+    DetailScreen:Movie
+}
+
+const Main = createStackNavigator<RootStackParams>();
 
 const MainNavigator = () => {
     return (
