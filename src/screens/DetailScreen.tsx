469b476bfadf2c9fcc5218283033414359f2ab6f
ing-arriola <ingeniero.arriola@gmail.com>
2021-11-25 07:57:13 -0500

    Added a poster image

--- src/screens/DetailScreen.tsx
+++ src/screens/DetailScreen.tsx
@@ -1,20 +1,34 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Image, StyleSheet, Dimensions } from 'react-native';
 import { StackScreenProps } from '@react-navigation/stack';
 import { RootStackParams } from '../navigation/MainNavigator';
 
 interface Props extends StackScreenProps<RootStackParams,'DetailScreen'>{}
 
+const screenHeight= Dimensions.get('screen').height
+
 const DetailScreen = ({route}:Props) => {
     const movie=route.params
-    console.log(movie)
+    const uri =`https://image.tmdb.org/t/p/w500${movie.poster_path}`
+    console.log(uri)
     return (
-        <View>
-            <Text>
-            DetailScreen
-            </Text>
+        <View style={styles.imageContainer}>
+            <Image
+                source={{uri}}
+                style={styles.posterImage}
+            />
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    imageContainer:{
+        width:'100%',
+        height:screenHeight *0.7
+    },
+    posterImage:{
+        flex:1
+    }
+});
 
 export default DetailScreen
