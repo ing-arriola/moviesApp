590cda112b82b367efa4c47e1447f837f2420f38
ing-arriola <ingeniero.arriola@gmail.com>
2021-11-27 19:15:47 -0500

    Added icon and more styles in image to get rid of some problems with the image's shadow

--- src/screens/DetailScreen.tsx
+++ src/screens/DetailScreen.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { View, Text, Image, StyleSheet, Dimensions } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons'
 import { StackScreenProps } from '@react-navigation/stack';
 import { RootStackParams } from '../navigation/MainNavigator';
 import { ScrollView } from 'react-native-gesture-handler';
@@ -11,28 +12,43 @@ const screenHeight= Dimensions.get('screen').height
 const DetailScreen = ({route}:Props) => {
     const movie=route.params
     const uri =`https://image.tmdb.org/t/p/w500${movie.poster_path}`
-    console.log(uri)
+    console.log(movie.id)
     return (
         <ScrollView>
             <View style={styles.imageContainer}>
-                <Image
-                    source={{uri}}
-                    style={styles.posterImage}
-                />
+                <View style={styles.imageBorder} >
+                    <Image
+                        source={{uri}}
+                        style={styles.posterImage}
+                    />
+                </View>
             </View>
             <View style={styles.marginContainer} >
                 <Text style={styles.title} >
                     {movie.title}
                 </Text>
             </View>
+            <View style={styles.marginContainer} >
+                <Icon 
+                    name='star-outline'
+                    color='grey'
+                    size={20}
+
+                />
+            </View>
         </ScrollView>
     )
 }
 
 const styles = StyleSheet.create({
+    imageBorder:{
+        flex:1,
+        borderBottomEndRadius:25,
+        borderBottomStartRadius:25,
+        overflow:'hidden',
+    },
     imageContainer:{
         width:'100%',
-        overflow:'hidden',
         height:screenHeight *0.7,
         shadowColor: "#000",
         shadowOffset: {
@@ -43,7 +59,7 @@ const styles = StyleSheet.create({
         shadowRadius: 7,
         elevation: 11,
         borderBottomEndRadius:25,
-        borderBottomStartRadius:25
+        borderBottomStartRadius:25,
     },
     posterImage:{
         flex:1
