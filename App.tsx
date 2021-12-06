2abadc87fe8b88ba10ae8f52d2b085fa9a82992a
ing-arriola <ingeniero.arriola@gmail.com>
2021-12-06 06:05:26 -0500

    wrapped the whole app into a context

--- App.tsx
+++ App.tsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native';
 import MainNavigator from './src/navigation/MainNavigator';
+import { GradientProvider } from './src/context/GradientContext';
+
+
+const AppState = ({children}:any) => {
+  return(
+    <GradientProvider>
+      {children}
+    </GradientProvider>
+  )
+}
 
 const App = () => {
   return (
     <NavigationContainer>
-      <MainNavigator/>
+      <AppState>
+        <MainNavigator/>
+      </AppState>
     </NavigationContainer>
   )
 }
