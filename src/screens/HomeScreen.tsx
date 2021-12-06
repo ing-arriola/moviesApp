31ae40eeb5f4f3d7c3ce4a9bf150f6fed5ce0d91
ing-arriola <ingeniero.arriola@gmail.com>
2021-12-06 06:06:19 -0500

    using context to set  background colors

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { View, ActivityIndicator, Dimensions } from 'react-native'
 import { ScrollView } from 'react-native-gesture-handler';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
@@ -9,11 +9,14 @@ import HorizontalCarrousel from '../components/HorizontalCarrousel';
 import { useMovies } from '../hooks/useMovies';
 import MoviePoster from './MoviePoster';
 import {getImageColors} from '../helpers/getColors'
+import { GradientContext } from '../context/GradientContext';
 
 const HomeScreen = () => {
     const { width:windowWidth } = Dimensions.get('window')
     const { nowInTheaters,popularMovies, topRated, upcoming,isLoading } = useMovies()
     const { top } = useSafeAreaInsets()
+    const {setMainColors} = useContext(GradientContext)
+    
 
     if(isLoading){
         return(
@@ -26,8 +29,8 @@ const HomeScreen = () => {
     const getPosterColors = async (index:number) => {
         const movie=nowInTheaters[index]
         const uri =`https://image.tmdb.org/t/p/w500${movie.poster_path}`
-        const [primary,secondary] = await getImageColors( uri ) 
-        console.log({primary,secondary})   
+        const [primary= "green",secondary="orange"] = await getImageColors( uri ) 
+        setMainColors({primary,secondary})   
     }
       
     return (
