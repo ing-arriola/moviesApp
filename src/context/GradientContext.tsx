f11d5ffb5c8c1bd8ab29dd76494c02b363fa7f29
ing-arriola <ingeniero.arriola@gmail.com>
2021-12-06 06:04:19 -0500

    Added context to pass colors

---
+++ src/context/GradientContext.tsx
@@ -0,0 +1,50 @@
+import React,{ createContext, useState } from "react";
+
+interface ImageColors {
+    primary:string;
+    secondary:string;
+}
+
+interface ContextProps{
+    colors:ImageColors;
+    prevColors:ImageColors;
+    setMainColors: (colors: ImageColors) => void; 
+    setPrevMainColors: (colors: ImageColors) => void;
+}
+
+export const GradientContext = createContext({} as ContextProps ) //TODO : DEFINE TYPE
+
+export const GradientProvider = ( { children } : any) => {
+    const [colors, setColors] = useState<ImageColors>({
+        primary:'red',
+        secondary:'blue'
+    })
+    const [prevColors, setprevColors] = useState<ImageColors>({
+        primary:'transparent',
+        secondary:'transparent'
+    })
+
+    const setMainColors = (colors:ImageColors) => {
+        setColors(colors)
+    }
+
+    const setPrevMainColors = (colors:ImageColors) => {
+        setprevColors(colors)
+    }
+
+
+    return(
+        <GradientContext.Provider
+            value={{
+                colors,
+                prevColors,
+                setMainColors,
+                setPrevMainColors
+            }}
+        >
+            { children }
+        </GradientContext.Provider>
+    )
+
+
+}
